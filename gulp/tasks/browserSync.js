ad31266fb175e4bea796d573385281ac07407afe
Bashar Ayyash <bashar.ayyash@gmail.com>
2015-10-20 14:44:28 +0300

    code alignment

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -1,4 +1,3 @@
-
 import config      from '../config';
 import browserSync from 'browser-sync';
 import gulp        from 'gulp';
@@ -6,9 +5,9 @@ import gulp        from 'gulp';
 gulp.task('browserSync', () => {
 
   browserSync({
-  	port: config.browserPort,
-  	ui: {
-    	port: config.UIPort
+    port: config.browserPort,
+    ui: {
+      port: config.UIPort
     },
     proxy: 'localhost:' + config.serverPort
   });
