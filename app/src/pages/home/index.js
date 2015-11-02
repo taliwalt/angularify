4746eabdbe830922ad6363becc87d0f4c0011abf
Bashar Ayyash <bashar.ayyash@gmail.com>
2015-11-02 12:46:10 +0200

    AppDirectives

--- app/src/pages/home/index.js
+++ app/src/pages/home/index.js
@@ -3,10 +3,10 @@ import uirouter from 'angular-ui-router';
 
 import routing from './home.routes.js';
 import HomeCtlr from './home.controller.js';
-import GreetingDirective from '../../directives/greeting';
+import AppDirectives from '../../directives';
 import Services from '../../services';
 
-export default angular.module('app.pages.home', [uirouter, GreetingDirective, Services])
+export default angular.module('app.pages.home', [uirouter, AppDirectives, Services])
   .config(routing)
   .controller('HomeController', HomeCtlr)
   .name;
