b8bd17a7638d4e176e7189a5fbcdfa9164c99525
Bashar Ayyash <bashar.ayyash@gmail.com>
2015-11-02 16:54:01 +0200

    include footerComponent

--- app/src/directives/index.js
+++ app/src/directives/index.js
@@ -1,6 +1,9 @@
 
 import angular from 'angular';
-import greetingDirective from './greeting';
+import greetingDirective from './greeting/greeting.directive.js';
+import footerComponent from './footer/footer.directive.js';
 
-export default angular.module('app.directives', [greetingDirective])
+export default angular.module('app.directives', [])
+  .directive('greeting', greetingDirective)
+  .directive('footerComponent', footerComponent)
   .name;
