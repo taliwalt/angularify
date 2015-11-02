c2010609dd7e263b548fc37e17b26f1a487ee87b
Bashar Ayyash <bashar.ayyash@gmail.com>
2015-11-02 17:06:17 +0200

    mov AppComponents dependency to app

--- app/src/pages/home/index.js
+++ app/src/pages/home/index.js
@@ -3,10 +3,9 @@ import uirouter from 'angular-ui-router';
 
 import routing from './home.routes.js';
 import HomeCtlr from './home.controller.js';
-import AppDirectives from '../../directives';
 import Services from '../../services';
 
-export default angular.module('app.pages.home', [uirouter, AppDirectives, Services])
+export default angular.module('app.pages.home', [uirouter, Services])
   .config(routing)
   .controller('HomeController', HomeCtlr)
   .name;
