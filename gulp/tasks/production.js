196174013c4e42ffaa139cafbceb6aeecf5ad531
Bashar Ayyash <bashar.ayyash@gmail.com>
2015-11-02 17:09:52 +0200

    change libs task position

--- gulp/tasks/production.js
+++ gulp/tasks/production.js
@@ -1,12 +1,12 @@
 import gulp        from 'gulp';
 import runSequence from 'run-sequence';
 
-gulp.task('prod', ['clean', 'libs'], function (cb) {
+gulp.task('prod', ['clean'], function (cb) {
 
   cb = cb || function () { };
 
   global.isProd = true;
 
-  runSequence(['styles', 'images', 'fonts', 'views', 'browserify'], 'gzip', cb);
+  runSequence(['styles', 'libs', 'images', 'fonts', 'views', 'browserify'], 'gzip', cb);
 
 });
