8d358e130e2c34b982b8d8c5967a16d7c186a433
Bashar Ayyash <bashar.ayyash@gmail.com>
2015-11-02 17:05:42 +0200

    add AppComponents dependency

--- app/src/app.js
+++ app/src/app.js
@@ -5,6 +5,7 @@ import uirouter from 'angular-ui-router';
 import routing from './app.config.js';
 import pages from './pages';
 import templates from './templates.js';
+import AppComponents from './directives';
 
-angular.module('app', [uirouter, pages, templates])
+angular.module('app', [uirouter, pages, templates, AppComponents])
   .config(routing);
