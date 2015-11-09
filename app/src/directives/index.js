4d1d501dda06723d165e9d68a38e6507b76b323a
Bashar Ayyash <bashar.ayyash@gmail.com>
2015-11-09 11:31:39 +0200

    delete footer component

--- app/src/directives/index.js
+++ app/src/directives/index.js
@@ -1,9 +1,7 @@
 
 import angular from 'angular';
 import greetingDirective from './greeting/greeting.directive.js';
-import footerComponent from './footer/footer.directive.js';
 
 export default angular.module('app.directives', [])
   .directive('greeting', greetingDirective)
-  .directive('footerComponent', footerComponent)
   .name;
